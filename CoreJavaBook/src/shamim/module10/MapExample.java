8a76953f2bce7b289c0c9cb1884be4d98bb61e8d
shamimsjava <shamimsjava@gmail.com>
2017-10-01 11:51:37 +0600

    Map example in collection framework

--- CoreJavaBook/src/shamim/module10/MapExample.java
+++ CoreJavaBook/src/shamim/module10/MapExample.java
@@ -1,5 +1,25 @@
 package shamim.module10;
 
+import java.util.*;
+
 public class MapExample {
-    
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("one", "1st");
+        map.put("second",new Integer(2));
+        map.put("third", "3rd");
+        map.put("third", "III");
+        
+        System.out.println(map);
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.entrySet());
+        
+        Collection collection = map.values();
+        Set set1 = map.keySet();
+        Set set2 = map.entrySet();
+        System.out.println(collection);
+        System.out.println(set1);
+        System.out.println(set2);
+    }
 }
