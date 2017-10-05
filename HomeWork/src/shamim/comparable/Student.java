2713ad2cacd567ca428d59e2bc11511158669984
shamimsjava <shamimsjava@gmail.com>
2017-10-05 07:45:55 +0600

    Home Work

---
+++ HomeWork/src/shamim/comparable/Student.java
@@ -0,0 +1,25 @@
+package shamim.comparable;
+
+public class Student implements Comparable<Student>{
+    String firstName, lastName;
+    int studentID;
+    double GPA;
+
+    public Student(String firstName, String lastName, int studentID, double GPA) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentID = studentID;
+        this.GPA = GPA;
+    }
+
+    @Override
+    public int compareTo(Student st) {
+        if(GPA == st.GPA)
+            return 0;
+        else if(GPA > st.GPA)
+            return -1;
+        else
+            return 1;
+    }
+    
+}
