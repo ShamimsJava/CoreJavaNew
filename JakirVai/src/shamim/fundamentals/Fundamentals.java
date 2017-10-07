7b639b86bd70589472c40695a45cb02b4e19e4c9
shamimsjava <shamimsjava@gmail.com>
2017-10-07 19:31:12 +0600

    Zakir vai

---
+++ JakirVai/src/shamim/fundamentals/Fundamentals.java
@@ -0,0 +1,11 @@
+package shamim.fundamentals;
+
+public class Fundamentals {
+    public int sum;
+    
+    public int add(int x, int y){
+        sum = x+y;
+        return sum;
+    }
+    
+}
---
+++ JakirVai/src/shamim/fundamentals/Multiplication.java
@@ -0,0 +1,13 @@
+package shamim.fundamentals;
+
+import java.util.Date;
+
+public class Multiplication {
+    public int mul;
+    public Date date;
+    
+    public int mult(int x, int y){
+        mul = x*y;
+        return mul;
+    }
+}
