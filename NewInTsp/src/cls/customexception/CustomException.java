f0c397e713975f0ce347f3697bd455cb9dff1afe
ShamimsJava <shamimsjava@gmail.com>
2017-10-08 09:31:03 +0600

    Class work

---
+++ NewInTsp/src/cls/customexception/CustomException.java
@@ -0,0 +1,26 @@
+package cls.customexception;
+
+public class CustomException extends Exception {
+    private String message = null;
+
+    public CustomException() {
+        super();
+    }
+    
+    public CustomException (String message){
+        super(message);
+        this.message = message;
+    }
+    
+    public CustomException(Throwable cause){
+        super(cause);
+    }
+    @Override
+    public String toString(){
+        return message;
+    }
+    @Override
+    public String getMessage(){
+        return message;
+    }
+}
